c36648da332f2556a4d59a7f4f2082dc6c1335f5
Spencer <spencerjholt@outlook.com>
2018-12-10 19:40:06 -0700

    forgot to commit

--- star.js
+++ star.js
@@ -46,21 +46,36 @@ shipContainer.appendChild(card)
 
 cardMaker(starships)
 
-function spaceship(name, model, ship_class, crew, hdr){
-    this.name = name
-    this.model = model
-    this.starship_class = ship_class
-    this.crew = crew
-    this.hyperdrive_rating = hdr
+//use arrow function here:
+class newShip {
+    constructor(name, model, ship_class, crew, hdr) {
+        this.name = name;
+        this.model = model;
+        this.starship_class = ship_class;
+        this.crew = crew;
+        this.hyperdrive_rating = hdr;
+    }
 }
+//ES6 style objects: made via classes. see codepen for the javascript examples we made.
+//add more objects. Make an array from new spaceship objects: will be more convincing.
+let ent = new newShip("Enterprise", "Constitution", "Cruiser", "250", "<10")
+let ser = new newShip("Serenity", "Firefly", "Class B Medium Transport", "5", "Unknown")
+let and = new newShip("Andromeda Ascendant", "Glorious Heritage", "Large Battlecruiser", "4500", "Slipstream Navigation")
+let pro = new newShip("Prometheus", "X-303", "Tau'ri Cruiser", "115", "Asguard Hyperdrive")
 
-let ent = new spaceship("Enterprise", "Constitution", "Cruiser", "250", "<10")
-
-console.log(ent)
-
+//console.log(ent)
+//Add more to this.
 const fed = [
-    ent
+    ent,
+    ser,
+    and,
+    pro
 ]
+//make more spaceships here. need more than one thing in an array to be convincing. Needs more of a purpose
+//Use more arrays: more meaningful. Make more star trek options to show up with the button: 
+
+//looking for iteration of arrays: need to add filter-map-reduce.
+//ie the harry potter project: sort by something.
 
 let bon = document.createElement("button");
 bon.innerHTML = "Another Spaceship?";
@@ -71,4 +86,19 @@ location.appendChild(bon)
 bon.addEventListener('click', () => {
     alert("Let's break canon, shall we?")
     cardMaker(fed)
+    
+    //if count === 4:
+    //have this do one spaceship at a time.
 });
+
+let bom = document.createElement("button");
+bom.innerHTML = "Crew Size Order";
+
+var location = document.getElementById("newCardButton")
+location.appendChild(bom)
+
+bon.addEventListener('click', () => {
+    
+});
+
+//Button that will filer/map by alphabetical order, one by crew capacity, maybe one by speed, and original.
